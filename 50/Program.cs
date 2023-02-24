1f8f8a7a62ddaae9b469f80c12abf77510c0490f
n0ser <unplay2004@gmail.com>
2023-02-24 22:30:26 +0600

    init commit

---
+++ 50/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Определить позицию элемента в двумерном массиве и найти его значение");
+Console.Write("Строка: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Столбец: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 3; 
+int m = 4; 
+
+Random random = new Random();
+
+int[,] arr = new int[n, m];
+
+Console.WriteLine("Исходный массив: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+{
+    arr[i, j] = random.Next(1, 10);
+Console.Write("{0} ", arr[i, j]);
+}
+Console.WriteLine();
+}
+    if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+{
+Console.WriteLine("Элемент не существует  ");
+}
+    else
+{
+    Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+}
+Console.ReadLine();
---
+++ 52/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Задали двумерный массив из целых чисел и нашли среднее арифметическое элементов в каждом СТОЛБЦЕ.");
+
+Random random = new Random();
+int[,] arr = new int[random.Next(2, 4), random.Next(2, 4)];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.Write("Среднее арифметическое каждого столбца: ");
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        sum += arr[i, j];
+    }
+    Console.Write($"{ sum / arr.GetLength(0)} ");
+}
+Console.ReadLine();
+
+
